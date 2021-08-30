6ddba8425065b3f1c6e97e21c19c908ce2186d91
AnnaPanosyan <80696532+AnnaPanosyan@users.noreply.github.com>
2021-08-30 12:53:59 -0700

    Update homework3.js

--- homework3.js
+++ homework3.js
@@ -78,3 +78,12 @@ function invert(obj) {
 // Create a function that builds a tree like object given an array with object which contains
 // parent and id properties.
 
+let treeNodes = [
+    { parent: null, id: 0 },
+    { parent: 0, id: 1 },
+    { parent: 0, id: 2 },
+    { parent: 1, id: 3 },
+    { parent: 1, id: 4 },
+    { parent: 2, id: 5 },
+    { parent: 4, id: 6 },
+  ];
